6591c6214529ade78dec1d04275b437a37554daa
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-18 14:15:10 -0400

    setup for game

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int easterEggCountdowm = 3;
         Scanner userInput;
-        String ynResponseStringType;
+        String ynResponse;
         userInput = new Scanner(System.in);
         boolean mainLoop = true;
         while (mainLoop == true) {
             System.out.println("do you want to try again?");
-            ynResponseStringType = userInput.next();
-            if (ynResponseStringType.equalsIgnoreCase("y")) {}
-            else if (ynResponseStringType.equalsIgnoreCase("n")) {
+            ynResponse = userInput.next();
+            if (ynResponse.equalsIgnoreCase("y")) {
+                System.out.println("hi");
+
+
+
+            }
+            else if (ynResponse.equalsIgnoreCase("n")) {
                 System.out.println("Goodbye then");
                 mainLoop = false;
             }
