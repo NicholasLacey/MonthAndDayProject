425fd76d20167c77d26940b7ed4003ebfa388186
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-18 14:03:39 -0400

    starting code added

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,26 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner userInput;
+        String ynResponseStringType;
+        userInput = new Scanner(System.in);
+        boolean mainLoop = true;
+        while (mainLoop == true) {
+            System.out.println("do you want to try again?");
+            ynResponseStringType = userInput.next();
+            if (ynResponseStringType.equalsIgnoreCase("y")) {}
+            else if (ynResponseStringType.equalsIgnoreCase("n")) {
+                System.out.println("Goodbye then");
+                mainLoop = false;
+            }
+
+
+
+
+        }
     }
 }
