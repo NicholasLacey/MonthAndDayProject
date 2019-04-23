a29b91669ad3036d028d4a4e51f7cf938a6fb041
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-23 14:02:30 -0400

    minor improvements
    place holder for until I get home

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int easterEggCountdowm = 3;
+        int easterEggCountdown = 3;
         Scanner userInput;
         String ynResponse;
         userInput = new Scanner(System.in);
         boolean mainLoop = true;
         while (mainLoop == true) {
-            System.out.println("do you want to try again?");
+            System.out.println("do you want to try again? y/n\n(single letter response please)");
             ynResponse = userInput.next();
             if (ynResponse.equalsIgnoreCase("y")) {
                 System.out.println("hi");
+                boolean secondaryLoop = true;
+                while (secondaryLoop == true) {
+
+                }
 
 
-
+                easterEggCountdown =easterEggCountdown-1;
             }
             else if (ynResponse.equalsIgnoreCase("n")) {
                 System.out.println("Goodbye then");
