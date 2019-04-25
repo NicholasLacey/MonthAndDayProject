3db1c4c13ab6093f9c0789f6ceca7ce73df0edaa
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-25 13:47:58 -0400

    start of Month and Day code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int easterEggCountdown = 3;
+        int easterEggCountdown = 6;
         Scanner userInput;
         String ynResponse;
         userInput = new Scanner(System.in);
@@ -14,10 +14,24 @@ public class Main {
             System.out.println("do you want to try again? y/n\n(single letter response please)");
             ynResponse = userInput.next();
             if (ynResponse.equalsIgnoreCase("y")) {
-                System.out.println("hi");
+                easterEggCountdown -= 0.5;
                 boolean secondaryLoop = true;
                 while (secondaryLoop == true) {
-
+                System.out.println("Monday's child is fair of face,\n" +
+                        "Tuesday's child is full of grace,\n" +
+                        "Wednesday's child is full of woe,\n" +
+                        "Thursday's child has far to go.\n" +
+                        "Friday's child is loving and giving,\n" +
+                        "Saturday's child works hard for a living,\n" +
+                        "But the child born on the Sabbath Day,\n" +
+                        "Is fair and wise and good in every way.\n\n to find out what you are please\ntype your b" +
+                        "irth year"+easterEggCountdown);
+                int year = userInput.nextInt();
+                System.out.println("now your birth month");
+                int month = userInput.nextInt();
+                System.out.println("now your birth day");
+                int day = userInput.nextInt();
+                    secondaryLoop = false;
                 }
 
 
